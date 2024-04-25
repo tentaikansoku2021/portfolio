82ae2cf2bedbc30349c837b2c1de30ce2b7c1a27
kazuhiko <k.tentaikansoku@gmail.com>
2024-04-25 19:01:55 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// To_Top
+const returnTop = document.querySelector('.return_top');
+window.addEventListener('scroll', ()=> {
+    let scroll_Y = window.scrollY;
+    if(scroll_Y >= 120) {
+        returnTop.classList.add('active');
+    }
+    else {
+        returnTop.classList.remove('active');
+    }
+})
+/* hamburger　menu */
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const half = document.querySelector(".half");
+const navLinks = document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    half.classList.toggle("nav-active")
+
+    navLinks.forEach((link,index) => {
+        if(link.style.animation) {
+            link.style.animation = "";
+        } else {
+            link.style.animation = `navLinksFade 0.5s ease forwards ${index/5+0.5}s`;
+        } 
+    });
+    burger.classList.toggle("toggle");
+    // navLinks.classList.toggle("clear");
+});
+
+nav.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    half.classList.toggle("nav-active");
+
+    navLinks.forEach((link,index) => {
+        if(link.style.animation) {
+            link.style.animation = "";
+        } else {
+            link.style.animation = `navLinksFade 0.5s ease forwards ${index/5+0.5}s`;
+        }
+    });  
+    burger.classList.toggle("toggle");  
+});
+
